35a2e892c7583c408901111d7a26bc50a5129ea9
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-07 00:57:58 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
 def meal_choice(veg1, veg2, protien = "tofu")
   puts "What a nutritious meal!"
-  return "A plate of #{protien} with #{broccoli} and #{macaroni}."
+  return "A plate of #{protien} with #{veg1} and #{veg2}."
 end
