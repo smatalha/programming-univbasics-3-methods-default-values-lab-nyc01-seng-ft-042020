0203fd4143fcd552272d06f3281c1f518893b0df
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-07 00:53:41 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
-def meal_choice = "protien"(broccoli, macaroni)
+def meal_choice(broccoli, macaroni)
   puts "What a nutritious meal!"
   return "A plate of tofu with #{broccoli} and #{macaroni}."
 end
